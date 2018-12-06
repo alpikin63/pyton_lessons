04db9bde8ce4060fae8c7f38766a898eee24afff
unknown <alpikin63@gmail.com>
2018-12-06 13:43:33 +0300

    3 lessons

--- login.py
+++ login.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
+import time
 
 
 class UntitledTestCase(unittest.TestCase):
+
     def setUp(self):
         self.driver = webdriver.Chrome()
-        self.driver.get("http://loginarea:passarea@st.gap.aeroidea.ru/")
+        self.driver.get("http://loginarea:passarea@gap.aeroidea.ru/")
 
     def test_untitled_test_case(self):
         driver = self.driver
-        driver.find_element_by_xpath(
-            u"(.//*[normalize-space(text()) and normalize-space(.)='Подробнее'])[1]/following::span[1]").click()
-        driver.find_element_by_xpath(
-            u"(.//*[normalize-space(text()) and normalize-space(.)='Войти'])[1]/following::a[1]").click()
+        self.open_authorization_form(driver)
+        self.authorization(driver, "alpikin63@gmail.com", '123456Qa!')
+        self.unauthorization(driver)
+
+    def unauthorization(self, driver):
+        driver.find_element_by_css_selector(
+            ".page-header__top-row-item.page-header__top-row-item--user.tooltip.js-tooltip").click()
+        driver.find_element_by_link_text("Выход").click()
+
+    def authorization(self, driver, login, password):
         driver.find_element_by_id("authEmail").click()
         driver.find_element_by_id("authEmail").clear()
-        driver.find_element_by_id("authEmail").send_keys("alpikin63@gmail.com")
+        driver.find_element_by_id("authEmail").send_keys(login)
         driver.find_element_by_id("authPassword").clear()
-        driver.find_element_by_id("authPassword").send_keys("123456Qa!")
-        # driver.find_element_by_link_text(u"войти").click()
-        # driver.find_element_by_xpath(
-        #     u"(.//*[normalize-space(text()) and normalize-space(.)='Подробнее'])[1]/following::span[1]").click()
-        # driver.find_element_by_link_text(u"Выход").click()
+        driver.find_element_by_id("authPassword").send_keys(password)
+        driver.find_element_by_css_selector(".popup-authorization .btn.btn--colored.js-submit").click()
+        time.sleep(5)
+
+    def open_authorization_form(self, driver):
+        driver.find_element_by_css_selector(
+            ".page-header__top-row-item.page-header__top-row-item--user.tooltip.js-tooltip").click()
+        driver.find_element_by_css_selector(".page-header__login-menu-enter.btn.btn--colored.js-modal").click()
 
     def tearDown(self):
         self.driver.quit()
